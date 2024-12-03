5393baef7ad35546599f4ffed30a8ce89846bd5f
Samson Varghese <samson@Samsons-MBP.broadband>
2024-12-03 23:11:31 +0000

    Added more scenarios

--- features/agify.feature
+++ features/agify.feature
@@ -1,7 +1,53 @@
-Feature: Get age by name using agify.io API
+Feature: Get age based on names using Agify.io API
+  As a user of Agify.io API,
+  I want to retrieve the estimated age for a given name,
+  So that I can gain insights into the age distribution of people with that name.
 
-  Scenario: Retrieve the age for a given name
-    Given I am an authenticated user
-    When I send a GET request
+  Scenario Outline: Retrieve the age for a valid name
+    Given the API is accessible
+    When I send a GET request with a valid '<name>'
     Then the API should respond with status code 200
-    And the response should include the age
+    And the response should include the '<name>'
+    And the response should include the estimated age
+    And the age should be a number
+    And the response should include a count of occurrences
+    And the count should be a number greater than 0
+
+    Examples:
+      | name     |
+      | Ava      |
+      | bob      | # All lowercase
+      | CHARLIE  | # All caps
+      | John Doe | # Full name
+      | José     | # Non-English name
+
+  Scenario: Retrieve ages for multiple names
+    Given the API is accessible
+    When I send a GET request with the names '<name1>', '<name2>' and '<name3>'
+    Then the API should respond with status code 200
+    And the response should include the estimated age for each name
+    And the response should include a count of occurrences for each name
+
+    Examples:
+      | name1 | name2 | name3 |
+      | Ava   | Emma  | Liam  |
+
+  Scenario: Verify that the API returns an error when name parameter is missing
+    Given the API is accessible
+    When I send a GET request without the name parameter
+    Then the API should respond with status code 422
+    And the response should include an error message indicating missing name parameter
+
+  Scenario: Verify that the API returns null age for an invalid name
+    Given the API is accessible
+    When I send a GET request with an invalid '<name>'
+    Then the API should respond with status code 200
+    And the response should include the '<name>'
+    And the age should be null
+    And the count should be 0
+
+    Examples:
+      | name         |
+      | invalid_name |
+      |          123 |
+      |              |
--- features/step_definitions/agifySteps.ts
+++ features/step_definitions/agifySteps.ts
@@ -3,10 +3,10 @@ const supertest = require("supertest");
 const request = supertest("https://api.agify.io");
 const assert = require("assert");
 
-Given("I am an authenticated user", () => {});
+Given("the API is accessible", () => {});
 
-When("I send a GET request", async function () {
-  this.response = await request.get("/?name=samson");
+When("I send a GET request with a valid {string}", async function (name) {
+  this.response = await request.get(`/?name=${name}`);
 });
 
 Then(
@@ -16,6 +16,76 @@ Then(
   }
 );
 
-Then("the response should include the age", async function () {
+Then("the response should include the {string}", async function (name) {
+  assert.ok(this.response.body.name === name);
+});
+
+Then("the response should include the estimated age", async function () {
   assert.ok(this.response.body.age);
 });
+
+Then("the age should be a number", async function () {
+  assert.ok(Number.isInteger(this.response.body.age));
+});
+
+Then("the response should include a count of occurrences", async function () {
+  assert.ok(this.response.body.count);
+});
+
+Then("the count should be a number greater than {int}", async function (count) {
+  assert.ok(this.response.body.count > count);
+});
+
+When("I send a GET request without the name parameter", async function () {
+  this.response = await request.get("/");
+});
+
+Then(
+  "the response should include an error message indicating missing name parameter",
+  async function () {
+    assert.ok(this.response.body.error === "Missing 'name' parameter");
+  }
+);
+
+When("I send a GET request with an invalid {string}", async function (name) {
+  this.response = await request.get(`/?name=${name}`);
+});
+
+Then("the age should be null", async function () {
+  assert.ok(this.response.body.age === null);
+});
+
+Then("the count should be {int}", async function (count) {
+  assert.ok(this.response.body.count === count);
+});
+
+When(
+  "I send a GET request with the names {string}, {string} and {string}",
+  async function (name1, name2, name3) {
+    this.response = await request.get(
+      `/?name[]=${name1}&name[]=${name2}&name[]=${name3}`
+    );
+  }
+);
+
+Then(
+  "the response should include the estimated age for each name",
+  async function () {
+    assert.ok(
+      Number.isInteger(this.response.body[0].age) &&
+        Number.isInteger(this.response.body[1].age) &&
+        Number.isInteger(this.response.body[2].age)
+    );
+  }
+);
+
+Then(
+  "the response should include a count of occurrences for each name",
+  async function () {
+    assert.ok(
+      this.response.body[0].count &&
+        this.response.body[1].count &&
+        this.response.body[2].count
+    );
+  }
+);
